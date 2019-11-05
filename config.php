9ed7abecf8a210caf9cad497a9b8a2f5d468b25a
Diego Najar <dignajar@gmail.com>
2019-11-05 21:56:17 +0100

    config file

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php defined('BLUDIT') or die('Bludit CMS.');
+
+// Disable all plugins
+deactivateAllPlugin();
+
+// Enable plugins
+activatePlugin('pluginTwitterCards');
+activatePlugin('pluginCanonical');
+activatePlugin('pluginDisqus');
+activatePlugin('pluginOpenGraph');
+activatePlugin('pluginRobots');
+activatePlugin('pluginRSS');
+activatePlugin('pluginSitemap');
+activatePlugin('pluginSitemap');
--- init.php
+++ init.php
@@ -1,1 +1,5 @@
 <?php
+
+if (isset($_GET['config'])) {
+	include_once(dirname(__FILE__).'/config.php');
+}
